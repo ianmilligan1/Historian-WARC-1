76bd3d5bb7a278256af38736a4ed1e984dcd62f0
ianmilligan1 <i2milligan@uwaterloo.ca>
2013-05-15 13:47:59 -0400

    Revised WARC-to-Stanford

--- WARC-to-Stanford.sh
+++ WARC-to-Stanford.sh
@@ -3,25 +3,22 @@
 
 # run in path of file
 # this is messy, creating several files (allowed me to find issues)
-# 
-# To use this, run the previous file in this repository and then run this.
 
-## This first part takes the WARC-TOOLS output and puts it into a format happy for Stanford-Termite.
-## IMPORTANT NOTE: SO FAR, ONLY WORKS ON WARC FILES WITH ONE FILE. BUT IT IS CLOSE TO BEING ABLE TO HANDLE
-## MULTIPLE FILES.
+# This first part takes the WARC-TOOLS output and formats it into the right format for StanfordHCI/Termite.
+# For this format, each WARC component needs to be on one line with a file number before it.
 
 textutil -convert txt fulltext.html # converts output file into fulltext.txt
 
-#sed 's/[^     ]*      \(.*\)/\1/' fulltext.txt > fulltext1.txt # sed accepts real tab space, copy and paste does not work
-#sed 's/[^     ]*      \(.*\)/\1/' fulltext1.txt > fulltext2.txt # ibid
+sed  's .\{4\}  ' fulltext.txt > fulltext2.txt # eliminates starting tabs
 
-sed  's .\{4\}  ' fulltext.txt > fulltext2.txt #  eliminates starting tabs
+cat -n fulltext2.txt | perl -pe "s/^\s*(\d+)\s+/\1\t/" > fulltext3.txt # introduces line numbers
 
-echo "1\t" | cat - fulltext2.txt > /tmp/out && mv /tmp/out output.txt # append the file number to front
+sed -i.old $'s/\xE2\x80\xA8/ /g' fulltext3.txt # removes weird unicode (hex 2028) line break
 
-tr -d '\r\n' <output.txt > output2.txt # eliminate line breaks
+# Some WARC files are too big, so you may need to truncate number of lines depending on memory.
+# default below takes first five files (1,5p) but replace upwards to change range.
 
-sed -i.old $'s/\xE2\x80\xA8/ /g' output2.txt # eliminates weird unicode (hex 2028) line break
+sed -n 1,5p fulltext3.txt > lines.txt 
 
 ## This file is now ready to be run with StanfordHCI/termite [https://github.com/StanfordHCI/termite]
 ## For StanfordHCI, create a config file pointing at output2.txt and execute
